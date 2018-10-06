8364c5d2985d97f0a3df5cd90ed31f582cc355ba
fakhri <fakhri.abdr@gmail.com>
2018-10-06 18:09:48 +0700

    Revert plural mode

--- src/channel/user/channelUser.ts
+++ src/channel/user/channelUser.ts
@@ -1,7 +1,7 @@
 import * as Sequelize from "sequelize";
 import { sequelize } from "../../sequelize";
 
-export const ChannelUser = sequelize.define("channel_users", {
+export const ChannelUser = sequelize.define("channel_user", {
   channelId: Sequelize.STRING,
   userId: Sequelize.STRING
 });
